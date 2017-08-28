b3d0d2898a4c5421fd06e73ef6427a359d51a552
Sir. Squiddle <thomas.passelegue@gmail.com>
2017-08-28 08:54:45 -0400

    Conversion en sortie console

--- src/TP1BDCS/TestDiagnostique.java
+++ src/TP1BDCS/TestDiagnostique.java
@@ -3,14 +3,20 @@ package TP1BDCS;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TestDiagnostique
 {
+    static final float taux_CAD_vers_EUR = (float)0.67153;
+    static final float taux_CAD_vers_USD = (float)0.80153;
+    static final float taux_CAD_vers_BTC = (float)0.00023;
+
     public static void main(String[] args)
     {
+
         /* Initialiser la fenêtre */
         JFrame fenetre = new JFrame();
-        fenetre.setSize(500,100);
+        fenetre.setSize(500,200);
 
         JPanel panneauPrincipal = new JPanel();
         panneauPrincipal.setLayout(new BoxLayout(panneauPrincipal, BoxLayout.Y_AXIS));
@@ -24,7 +30,30 @@ public class TestDiagnostique
         JButton validerConversion = new JButton("Convertir");
         JLabel resultConv = new JLabel("Résultat");
 
-        validerConversion.addActionListener();
+        validerConversion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String montantTexte = montant.getText();
+                float montantNum = Integer.parseInt(montantTexte);
+
+                String devise = (String)selectDevise.getSelectedItem();
+                float montantConverti = 0;
+                if(devise.compareTo("USD")==0)
+                {
+                    montantConverti = montantNum*taux_CAD_vers_USD;
+
+                }else if(devise.compareTo("EUR")==0)
+                {
+                    montantConverti = montantNum*taux_CAD_vers_EUR;
+
+                }else if(devise.compareTo("BTC")==0)
+                {
+                    montantConverti = montantNum*taux_CAD_vers_BTC;
+
+                }
+                System.out.println("le montant converti est "+montantConverti);
+            }
+        });
 
 
         GridBagConstraints contrainte = new GridBagConstraints();
@@ -43,24 +72,16 @@ public class TestDiagnostique
         contrainte.gridy=2;
         panneauPrincipal.add(selectDevise, contrainte);
 
-        contrainte.gridx=0;
-        contrainte.gridy=3;
+        contrainte.gridx=1;
+        contrainte.gridy=0;
         panneauPrincipal.add(resultConv, contrainte);
 
-        contrainte.gridx=0;
-        contrainte.gridy=4;
+        contrainte.gridx=1;
+        contrainte.gridy=1;
         panneauPrincipal.add(validerConversion, contrainte);
 
         fenetre.setContentPane(panneauPrincipal);
         fenetre.setVisible(true);
     }
 
-
-    public void actionPerformed(ActionEvent e) {
-       /*if(e.getSource()==validerConversion)
-       {
-
-       }*/
-       System.out.println(e.getSource());
-    }
 }
